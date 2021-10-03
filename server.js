d1a82d6c906ff3ccc2ee0658d513b33079ce99aa
ky04071220 <83106486+ky04071220@users.noreply.github.com>
2021-10-03 12:10:19 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const city = document.getElementById('select1')
+const array = document.getElementById('select2')
+const go = document.getElementById('select3')
+ 
+const listContent = []
+
+
+
+window.addEventListener('loadfinBtn',function(){
+
+  const userchoose = {
+    city ='select1',
+    array ='select2',
+    go ='select3',
+  }
+  const userchoose=['使用者選擇',userchoose]
+  
+})
