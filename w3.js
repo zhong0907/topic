34a875d95ac2846ac0b226c7a6260419f0f26b5a
ky04071220 <83106486+ky04071220@users.noreply.github.com>
2021-10-03 12:07:22 +0800

    Create w3.js

---
+++ w3.js
@@ -0,0 +1,14 @@
+
+var testInput = document.getElementById("testInput");
+var submitBtn = document.querySelector(".submitBtn");
+
+function FsubmitBtn(value) {
+  var str = "tmper";
+  var str = "locat";
+  var submitValue = testInput.value;
+  str = submitValue;
+  alert(str);
+}
+
+submitBtn.addEventListener("sendOut", FsubmitBtn);
+submitBtn.addEventListener("clearUp", FsubmitBtn);
